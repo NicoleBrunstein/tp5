f693ea7278e04596888414c88ddcd1719d46663a
Android Studio <devandroid@ort>
2024-05-20 11:54:39 -0300

    2

--- TP04/src/controllers/event_categories-controller.js
+++ TP04/src/controllers/event_categories-controller.js
@@ -46,3 +46,5 @@ router.delete('/:id', async (req, res) => {
 });
 
 export default router;
+
+
--- TP04/src/repositories/event_enrollments-repository.js
+++ TP04/src/repositories/event_enrollments-repository.js
@@ -4,33 +4,25 @@ const {Client, Pool}=pkg;
 
 export default class event_enrollmentsRepository
 {
-    getAllAsync = async () => {
-        let returnArray = null;
+    
+    createAsync = async (entity) => {
+        console.log('event_categoriesRepository.createAsync(${JSON.stringify(entity)})');
         const client = new Client(DBConfig);
-        try {
-            await client.connect();
-            const sql = `SELECT * FROM event_enrollments`;
-            const result = await client.query(sql);
-            await client.end();
-            returnArray = result.rows;
-        } catch (error) {
-            console.log(error);
-        }
-        return returnArray;
-    }
+        let rowsAffected = 0;
 
-    getByNameAsync = async () => {
-        let returnArray = null;
-        const client = new Client(DBConfig);
-        try {
+        try{
             await client.connect();
-            const sql = `SELECT * FROM event_enrollments`;
-            const result = await client.query(sql);
-            await client.end();
-            returnArray = result.rows;
-        } catch (error) {
-            console.log(error);
+            const sql = 'INSERT INTO event_enrollments(id, id_event, id_user, description, registration_date_time, attended, observations, rating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+            const values = [entity.id, entity.id_event, entity.id_user, entity.description, entity.registration_date_time, entity.attended, entity.observations, entity.rating];
+            const result = await client.query(sql, values);
+            rowsAffected = result.affectedRows;
+        }catch (error){
+            
+            console.log(error)
         }
-        return returnArray;
+
+        return rowsAffected;
     }
+    
 }
+
