63dc789bfdb6c70aa40c139f4168b3350ae9123a
Android Studio <devandroid@ort>
2024-05-27 09:05:41 -0300

    hola

--- TP04/src/repositories/event_enrollments-repository.js
+++ TP04/src/repositories/event_enrollments-repository.js
@@ -24,12 +24,75 @@ export default class event_enrollmentsRepository
         return rowsAffected;
     }
 
-    getByIdAsync = async (id) => {
+    getByName = async (id, first_name) => {
         const client = new Client(DBConfig);
         await client.connect();
         let returnEntity = null;
-        const sql = `SELECT * FROM event_categories WHERE id = $1`;
-        const values = [id]
+        const sql = `SELECT * FROM event_enrollments WHERE first_name = $1`;
+        const values = [first_name]
+        const result = await client.query(sql, values);
+        if (result.rows.length > 0){
+            returnEntity = result.rows[0];
+        }
+        return  returnEntity;
+    }
+
+    
+    getByLastName = async (id, last_name) => {
+        const client = new Client(DBConfig);
+        await client.connect();
+        let returnEntity = null;
+
+
+        let  sql = `SELECT * FROM event_enrollments`;
+        sql  =  sql + `AND firsnamt=asdasd`;
+
+
+
+        sql  =  sql + `AND latsName=asdasd`;
+
+
+        const sql = `SELECT * FROM event_enrollments WHERE last_name = $1`;
+        
+        const values = [last_name]
+        const result = await client.query(sql, values);
+        if (result.rows.length > 0){
+            returnEntity = result.rows[0];
+        }
+        return  returnEntity;
+    }
+
+    getByUsername = async (id, username) => {
+        const client = new Client(DBConfig);
+        await client.connect();
+        let returnEntity = null;
+        const sql = `SELECT * FROM event_enrollments WHERE username = $1`;
+        const values = [username]
+        const result = await client.query(sql, values);
+        if (result.rows.length > 0){
+            returnEntity = result.rows[0];
+        }
+        return  returnEntity;
+    }
+    getByUsername = async (id, attended) => {
+        const client = new Client(DBConfig);
+        await client.connect();
+        let returnEntity = null;
+        const sql = `SELECT * FROM event_enrollments WHERE attended = $1`;
+        const values = [attended]
+        const result = await client.query(sql, values);
+        if (result.rows.length > 0){
+            returnEntity = result.rows[0];
+        }
+        return  returnEntity;
+    }
+
+    getByUsername = async (id, rating) => {
+        const client = new Client(DBConfig);
+        await client.connect();
+        let returnEntity = null;
+        const sql = `SELECT * FROM event_enrollments WHERE rating = $1`;
+        const values = [rating]
         const result = await client.query(sql, values);
         if (result.rows.length > 0){
             returnEntity = result.rows[0];
